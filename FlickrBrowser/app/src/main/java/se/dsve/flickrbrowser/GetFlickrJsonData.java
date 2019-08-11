44f68c14004ad03186ef4d485430f88359590781
Lars Strömberg <stromberg.lars@gmail.com>
2019-08-11 21:52:11 +0200

    Started to make a flicker app

---
+++ FlickrBrowser/app/src/main/java/se/dsve/flickrbrowser/GetFlickrJsonData.java
@@ -0,0 +1,82 @@
+package se.dsve.flickrbrowser;
+/*
+  __
+ /\ \
+ \_\ \    ____  __  __     __
+ /'_` \  /',__\/\ \/\ \  /'__`\
+/\ \L\ \/\__, `\ \ \_/ |/\  __/
+\ \___,_\/\____/\ \___/ \ \____\
+ \/__,_ /\/___/  \/__/   \/____/
+       https://dsve.se/
+*/
+
+import android.net.Uri;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Lars Strömberg
+ * @version 1.0
+ * @since 2019-08-02
+ * https://github.com/deskavaenkelt/
+ */
+class GetFlickrJsonData implements GetRawData.OnDownloadComplete {
+    private static final String TAG = "GetFlickrJsonData";
+
+    private List<Photo> mPhotoList = null;
+    private String mBaseURL;
+    private String mLanguage;
+    private boolean mMatchAll;
+
+    private final OnDataAvailable mCallBack;
+
+    interface OnDataAvailable {
+        void onDataAvailable(List<Photo> data, DownloadStatus status);
+    }
+
+    public GetFlickrJsonData(OnDataAvailable callBack, String baseURL, String language, boolean matchAll) {
+        Log.d(TAG, "GetFlickrJsonData: called");
+        mBaseURL = baseURL;
+        mLanguage = language;
+        mMatchAll = matchAll;
+        mCallBack = callBack;
+    }
+
+    void executeOnSameThread(String searchCriteria) {
+        Log.d(TAG, "executeOnSameThread: starts");
+        String destinationUri = createUri(searchCriteria, mLanguage, mMatchAll);
+
+        GetRawData getRawData = new GetRawData(this);
+        getRawData.execute(destinationUri);
+        Log.d(TAG, "executeOnSameThread: ends");
+    }
+
+    private String createUri(String searchCriteria, String lang, boolean matchAll) {
+        Log.d(TAG, "createUri: starts");
+
+        return Uri.parse(mBaseURL).buildUpon()
+                .appendQueryParameter("tags", searchCriteria)
+                .appendQueryParameter("tagmode", matchAll ? "ALL" : "ANY")
+                .appendQueryParameter("lang", lang)
+                .appendQueryParameter("format", "json")
+                .appendQueryParameter("nojsoncallback", "1")
+                .build().toString();
+    }
+
+    @Override
+    public void onDownloadComplete(String data, DownloadStatus status) {
+        Log.d(TAG, "onDownloadComplete: starts. Status = " + status);
+
+        if (status == DownloadStatus.OK) {
+            mPhotoList = new ArrayList<>();
+
+        }
+    }
+}
+
+
+
+
+
---
+++ FlickrBrowser/app/src/main/java/se/dsve/flickrbrowser/Photo.java
@@ -0,0 +1,72 @@
+package se.dsve.flickrbrowser;
+/*
+  __
+ /\ \
+ \_\ \    ____  __  __     __
+ /'_` \  /',__\/\ \/\ \  /'__`\
+/\ \L\ \/\__, `\ \ \_/ |/\  __/
+\ \___,_\/\____/\ \___/ \ \____\
+ \/__,_ /\/___/  \/__/   \/____/
+       https://dsve.se/
+*/
+
+/**
+ * @author Lars Strömberg
+ * @version 1.0
+ * @since 2019-08-02
+ * https://github.com/deskavaenkelt/
+ */
+class Photo {
+
+    private String mTitle;
+    private String mAuthor;
+    private String mAuthorId;
+    private String mLink;
+    private String mTags;
+    private String mImage;
+
+    public Photo(String title, String author, String authorId, String link, String tags, String image) {
+        mTitle = title;
+        mAuthor = author;
+        mAuthorId = authorId;
+        mLink = link;
+        mTags = tags;
+        mImage = image;
+    }
+
+    String getTitle() {
+        return mTitle;
+    }
+
+    String getAuthor() {
+        return mAuthor;
+    }
+
+    String getAuthorId() {
+        return mAuthorId;
+    }
+
+    String getLink() {
+        return mLink;
+    }
+
+    String getTags() {
+        return mTags;
+    }
+
+    String getImage() {
+        return mImage;
+    }
+
+    @Override
+    public String toString() {
+        return "Photo{" +
+                "mTitle='" + mTitle + '\'' +
+                ", mAuthor='" + mAuthor + '\'' +
+                ", mAuthorId='" + mAuthorId + '\'' +
+                ", mLink='" + mLink + '\'' +
+                ", mTags='" + mTags + '\'' +
+                ", mImage='" + mImage + '\'' +
+                '}';
+    }
+}
